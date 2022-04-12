d1c73d7676ca55274e807e590103f232030ecfd4
keke <dondohare55@gmail.com>
2022-04-12 20:44:27 +0900

    landing skelton

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -38,102 +38,105 @@ const Layout: React.FC<Props> = ({ children }) => {
           content={metaDescription}
         />
       </Head>
-      <div>
-        <Disclosure as="nav" className="bg-gray-800">
-          {({ open }) => (
-            <>
-              <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-                <div className="flex items-center justify-between h-16">
-                  <div className="flex items-center">
-                    <div className="flex-shrink-0">
-                      <Link href="/">
-                        <a className="text-white px-3 py-2 rounded-md text-lg font-medium">
-                          DANDORI
-                        </a>
-                      </Link>
-                    </div>
-                    <div className="hidden md:block">
-                      <div className="ml-10 flex items-baseline space-x-4">
-                        {navigation.map((item) => (
-                          <Link key={item.name} href={item.path}>
-                            <a
-                              className={classNames(
-                                router.pathname === item.path
-                                  ? 'bg-gray-900 text-white'
-                                  : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                                'px-3 py-2 rounded-md text-sm font-medium'
-                              )}
-                            >
-                              {item.name}
-                            </a>
-                          </Link>
-                        ))}
-                        <button
-                          className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-                          onClick={() => {
-                            dispatch(signOut());
-                            toast.success('ログアウトしました');
-                          }}
-                        >
-                          ログアウト
-                        </button>
+      {router.pathname === '/' && <main>{children}</main>}
+      {router.pathname !== '/' && (
+        <div>
+          <Disclosure as="nav" className="bg-gray-800">
+            {({ open }) => (
+              <>
+                <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+                  <div className="flex items-center justify-between h-16">
+                    <div className="flex items-center">
+                      <div className="flex-shrink-0">
+                        <Link href="/">
+                          <a className="text-white px-3 py-2 rounded-md text-lg font-medium">
+                            DANDORI
+                          </a>
+                        </Link>
+                      </div>
+                      <div className="hidden md:block">
+                        <div className="ml-10 flex items-baseline space-x-4">
+                          {navigation.map((item) => (
+                            <Link key={item.name} href={item.path}>
+                              <a
+                                className={classNames(
+                                  router.pathname === item.path
+                                    ? 'bg-gray-900 text-white'
+                                    : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                                  'px-3 py-2 rounded-md text-sm font-medium'
+                                )}
+                              >
+                                {item.name}
+                              </a>
+                            </Link>
+                          ))}
+                          <button
+                            className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                            onClick={() => {
+                              dispatch(signOut());
+                              toast.success('ログアウトしました');
+                            }}
+                          >
+                            ログアウト
+                          </button>
+                        </div>
                       </div>
                     </div>
-                  </div>
-                  <div className="-mr-2 flex md:hidden">
-                    {/* Mobile menu button */}
-                    <Disclosure.Button className="bg-gray-800 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
-                      <span className="sr-only">Open main menu</span>
-                      {open ? (
-                        <XIcon className="block h-6 w-6" aria-hidden="true" />
-                      ) : (
-                        <MenuIcon
-                          className="block h-6 w-6"
-                          aria-hidden="true"
-                        />
-                      )}
-                    </Disclosure.Button>
+                    <div className="-mr-2 flex md:hidden">
+                      {/* Mobile menu button */}
+                      <Disclosure.Button className="bg-gray-800 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
+                        <span className="sr-only">Open main menu</span>
+                        {open ? (
+                          <XIcon className="block h-6 w-6" aria-hidden="true" />
+                        ) : (
+                          <MenuIcon
+                            className="block h-6 w-6"
+                            aria-hidden="true"
+                          />
+                        )}
+                      </Disclosure.Button>
+                    </div>
                   </div>
                 </div>
-              </div>
 
-              <Disclosure.Panel className="md:hidden">
-                <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
-                  {navigation.map((item) => (
-                    <a
-                      key={item.name}
-                      href={item.path}
-                      className={classNames(
-                        router.pathname === item.path
-                          ? 'bg-gray-900 text-white'
-                          : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                        'block px-3 py-2 rounded-md text-base font-medium'
-                      )}
+                <Disclosure.Panel className="md:hidden">
+                  <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
+                    {navigation.map((item) => (
+                      <a
+                        key={item.name}
+                        href={item.path}
+                        className={classNames(
+                          router.pathname === item.path
+                            ? 'bg-gray-900 text-white'
+                            : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                          'block px-3 py-2 rounded-md text-base font-medium'
+                        )}
+                      >
+                        {item.name}
+                      </a>
+                    ))}
+                    <span
+                      className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium cursor-pointer"
+                      onClick={() => {
+                        dispatch(signOut());
+                        toast.success('ログアウトしました');
+                      }}
                     >
-                      {item.name}
-                    </a>
-                  ))}
-                  <span
-                    className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium cursor-pointer"
-                    onClick={() => {
-                      dispatch(signOut());
-                      toast.success('ログアウトしました');
-                    }}
-                  >
-                    ログアウト
-                  </span>
-                </div>
-              </Disclosure.Panel>
-            </>
-          )}
-        </Disclosure>
+                      ログアウト
+                    </span>
+                  </div>
+                </Disclosure.Panel>
+              </>
+            )}
+          </Disclosure>
 
-        <main>
-          <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-            {children}
-          </div>
-        </main>
-      </div>
+          <main>
+            <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+              {children}
+            </div>
+          </main>
+        </div>
+      )}
     </>
   );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,131 @@
 import type { NextPage } from 'next';
+import React, { useEffect } from 'react';
+import AOS from 'aos';
+import Link from 'next/link';
+import Button from '../components/common/Button';
+import Image from 'next/image';
+import 'aos/dist/aos.css';
 
 const Home: NextPage = () => {
+  useEffect(() => {
+    AOS.init({
+      once: true,
+      easing: 'ease-out-sine',
+      duration: 600,
+    });
+  }, []);
+
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    <div className="pt-32 pb-12 px-6 md:pt-40 md:pb-20 md:px-8">
+      <div className="text-center pb-20 md:pb-24">
+        <h1
+          className="text-5xl md:text-6xl font-extrabold tracking-tight mb-8 leading-normal"
+          data-aos="zoom-y-out"
+        >
+          あなたの当たり前は {}
+          <span className="bg-clip-text text-transparent bg-gradient-to-r from-indigo-500 to-cyan-400">
+            誰かの特別
+          </span>
+          <span className="text-sm">……かもしれない</span>
+        </h1>
+        <div className="max-w-3xl mx-auto">
+          <p
+            className="text-xl text-gray-600 mb-6"
+            data-aos="zoom-y-out"
+            data-aos-delay="150"
+          >
+            自分の知識や経験をもとに、手順の作成＆公開をしてみよう。知識の共有の輪を広げよう。
+          </p>
+          <div
+            className="mx-auto sm:max-w-none sm:flex sm:justify-center"
+            data-aos="zoom-y-out"
+            data-aos-delay="300"
+          >
+            <div className="sm:mx-4">
+              <Link href={`/public_procedures`}>
+                <a>
+                  <Button text="公開されている手順を見てみる" color="blue" />
+                </a>
+              </Link>
+            </div>
+            <div className="sm:mx-4">
+              <Link href={`/sign_up`}>
+                <a>
+                  <Button text="ユーザー登録する" color="green" />
+                </a>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="text-center pb-12 md:pb-16">
+        <h2
+          className="text-4xl md:text-5xl font-bold tracking-widest leading-normal"
+          data-aos="zoom-y-out"
+        >
+          特長
+        </h2>
+        <div
+          className="grid grid-cols-2 gap-4 px-12 my-20"
+          data-aos="fade-right"
+        >
+          <div>
+            <h3 className="text-2xl md:text-3xl font-bold my-3">
+              マークダウン形式でスタイルを自由に設定
+            </h3>
+            <p className="text-lg text-gray-600">
+              タイトルや強調、箇条書きやハイパーリンクなどを手順の記載内容に自由に設定できます。
+            </p>
+          </div>
+          <Image
+            src="/app-sample-screen.gif"
+            alt="hero-image"
+            width={840}
+            height={619}
+          />
+        </div>
+        <div
+          className="grid grid-cols-2 gap-4 px-12 my-20"
+          data-aos="fade-left"
+        >
+          <Image
+            src="/600x400.png"
+            alt="dummy-image"
+            width={600}
+            height={400}
+          />
+          <div>
+            <h3 className="text-2xl md:text-3xl font-bold my-3">
+              twitterで簡単にシェア
+            </h3>
+            <p className="text-lg text-gray-600">
+              ボタンを押せば、公開中の投稿を簡単にシェアできます。
+            </p>
+          </div>
+        </div>
+        <div
+          className="grid grid-cols-2 gap-4 px-12 my-20"
+          data-aos="fade-right"
+        >
+          <div>
+            <h3 className="text-2xl md:text-3xl font-bold my-3">
+              Lorem ipsum dolor
+            </h3>
+            <p className="text-lg text-gray-600">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt
+              quas, maiores perferendis ducimus, quibusdam, harum qui a eligendi
+              fugiat aliquam atque laborum. Accusamus dolorem id alias rem error
+              minima facere?
+            </p>
+          </div>
+          <Image
+            src="/600x400.png"
+            alt="dummy-image"
+            width={600}
+            height={400}
+          />
+        </div>
+      </div>
     </div>
   );
 };
