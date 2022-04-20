00cbfd3b13ae4a27cefb30e27d43b1e1536b9fee
keke <dondohare55@gmail.com>
2022-04-20 11:24:39 +0900

    add username to detail page

--- pages/public_procedures/[id].tsx
+++ pages/public_procedures/[id].tsx
@@ -1,10 +1,10 @@
 import Head from 'next/head';
 import { GetServerSideProps } from 'next';
 import { ProcedureFormProps } from '../../types/Procedure';
-import MyProcedurePreview from '../../components/procedure/ProcedurePreview';
+import ProcedurePreview from '../../components/procedure/ProcedurePreview';
 
 type Props = {
-  procedure: ProcedureFormProps & { id: number };
+  procedure: ProcedureFormProps & { id: number; username: string };
 };
 
 const DetailProcedure: React.FC<Props> = ({ procedure }) => {
@@ -34,11 +34,28 @@ const DetailProcedure: React.FC<Props> = ({ procedure }) => {
 
       {procedure.publish ? (
         <div className="max-w-5xl w-full mx-auto z-10">
-          <MyProcedurePreview
+          <ProcedurePreview
             title={procedure.title}
             content={procedure.content}
             steps={procedure.steps}
           />
+          <div className="flex font-medium justify-end my-10">
+            <div className="inline-flex items-center">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
+                  clipRule="evenodd"
+                />
+              </svg>
+              <p>投稿者：{procedure.username}さん</p>
+            </div>
+          </div>
         </div>
       ) : (
         <>非公開につき閲覧できません</>
