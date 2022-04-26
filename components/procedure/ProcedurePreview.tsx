7b3bf99b54022557030f808c18bc7f450d15e812
keke <dondohare55@gmail.com>
2022-04-27 08:59:27 +0900

    nil guard

--- components/procedure/ProcedurePreview.tsx
+++ components/procedure/ProcedurePreview.tsx
@@ -10,29 +10,30 @@ const ProcedurePreview: React.FC<Props> = ({ title, content, steps }) => {
       <MarkDownView str={title} />
       <MarkDownView str={content} />
       <div>
-        {steps.map((step, index) => {
-          return (
-            <div key={index} className="my-10">
-              <h4 className="flex my-1 after:contents-[''] after:flex-auto after:border-b after:border-gray-300 after:my-auto after:ml-2">{`手順-${
-                index + 1
-              }`}</h4>
-              {step.dataUrl ? (
-                <div>
-                  <div className="flex justify-center">
-                    <img src={step.dataUrl} className="max-h-80 m-5" />
+        {steps &&
+          steps.map((step, index) => {
+            return (
+              <div key={index} className="my-10">
+                <h4 className="flex my-1 after:contents-[''] after:flex-auto after:border-b after:border-gray-300 after:my-auto after:ml-2">{`手順-${
+                  index + 1
+                }`}</h4>
+                {step.dataUrl ? (
+                  <div>
+                    <div className="flex justify-center">
+                      <img src={step.dataUrl} className="max-h-80 m-5" />
+                    </div>
                   </div>
-                </div>
-              ) : step.downloadUrl ? (
-                <div className="flex justify-center">
-                  <img src={step.downloadUrl} className="max-h-80 m-5" />
-                </div>
-              ) : (
-                <div></div>
-              )}
-              <MarkDownView str={step.content} />
-            </div>
-          );
-        })}
+                ) : step.downloadUrl ? (
+                  <div className="flex justify-center">
+                    <img src={step.downloadUrl} className="max-h-80 m-5" />
+                  </div>
+                ) : (
+                  <div></div>
+                )}
+                <MarkDownView str={step.content} />
+              </div>
+            );
+          })}
       </div>
     </>
   );
