32cf73d78847edc071bcb8a3bbdb8d2b67f32676
keke <dondohare55@gmail.com>
2022-05-01 20:39:14 +0900

    outline button

--- components/common/OutlineButton.tsx
+++ components/common/OutlineButton.tsx
@@ -12,7 +12,7 @@ type Props = {
   HTMLButtonElement
 >;
 
-const Button: React.FC<Props> = ({
+const OutlineButton: React.FC<Props> = ({
   color,
   text,
   fullWidth = false,
@@ -23,13 +23,13 @@ const Button: React.FC<Props> = ({
   const buttonColorNames = useMemo(() => {
     switch (color) {
       case 'green':
-        return 'bg-green-500 hover:bg-green-700';
+        return 'border-green-500 text-green-700 hover:border-green-700 ';
       case 'pink':
-        return 'bg-pink-500 hover:bg-pink-700';
+        return 'border-pink-500 text-pink-700 hover:border-pink-700';
       case 'gray':
-        return 'bg-gray-500 hover:bg-gray-700';
+        return 'border-gray-500 text-gray-700 hover:border-gray-700';
       default:
-        return 'bg-blue-500 hover:bg-blue-700';
+        return 'border-blue-500 text-blue-700 hover:border-blue-700';
     }
   }, [color]);
 
@@ -39,7 +39,7 @@ const Button: React.FC<Props> = ({
         type="button"
         className={`${fullWidth ? 'w-full max-w-5xl' : ''} ${
           disabled ? 'opacity-50 cursor-not-allowed' : ''
-        } ${buttonColorNames} text-white font-medium py-2 px-4 rounded inline-flex items-center justify-center `}
+        } ${buttonColorNames} font-normal py-2 px-4 rounded inline-flex items-center justify-center bg-transparent border-2`}
         disabled={disabled}
         {...rest}
       >
@@ -50,4 +50,4 @@ const Button: React.FC<Props> = ({
   );
 };
 
-export default Button;
+export default OutlineButton;
